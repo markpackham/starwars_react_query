49075fe00b85b3ea5c26594b7266ff1d800c271f
mark <markpackham1@gmail.com>
2021-10-16 21:20:03 +0100

    Output h1

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import People from "./components/People";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Planets />
-        <People />
+    <>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar />
+        <div className="content">
+          <Planets />
+          <People />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
