e9cfc0b4b22d614d6b77615ed79053211f565270
mark <markpackham1@gmail.com>
2021-10-17 17:08:00 +0100

    modify code to suit modern version of react-query

--- src/App.js
+++ src/App.js
@@ -3,25 +3,23 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import Navbar from "./components/Navbar";
 import Planets from "./components/Planets";
 import People from "./components/People";
-import { ReactQueryDevtools } from "react-query-devtools";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function App() {
   const queryClient = new QueryClient();
   let [page, setPage] = useState("planets");
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="App">
-        <QueryClientProvider client={queryClient}>
-          <h1>Star Wars Info</h1>
-          <Navbar setPage={setPage} />
-          <div className="content">
-            {page === "planets" ? <Planets /> : <People />}
-          </div>
-        </QueryClientProvider>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
-    </>
+    </QueryClientProvider>
   );
 }
 
