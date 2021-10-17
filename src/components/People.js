154114cedc0f8b08bdfa822d781eb077c422a181
mark <markpackham1@gmail.com>
2021-10-17 16:57:34 +0100

    fetch people
    https://youtu.be/-i3ZQP1yPEs?t=4

--- src/components/People.js
+++ src/components/People.js
@@ -1,10 +1,29 @@
+import { useQuery } from "react-query";
 import Person from "./Person";
 
+const fetchPeople = async () => {
+  const res = await fetch("http://swapi.dev/api/people/");
+  return res.json();
+};
+
 export default function People() {
+  const { data, status } = useQuery("people", fetchPeople);
+
   return (
     <div>
       <h2>People</h2>
-      <Person />
+
+      {status === "loading" && <div>Loading data</div>}
+
+      {status === "error" && <div>Error fetching data</div>}
+
+      {status === "success" && (
+        <div>
+          {data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Person.js
+++ src/components/Person.js
@@ -1,5 +1,9 @@
-import React from "react";
-
-export default function Person() {
-  return <div></div>;
+export default function Person({ person }) {
+  return (
+    <div className="card">
+      <h3>{person.name}</h3>
+      <p>Gender - {person.gender}</p>
+      <p>Birth year - {person.birth_year}</p>
+    </div>
+  );
 }
